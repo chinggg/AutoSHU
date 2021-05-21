e6120b840a0e400e5d095122bdfa2770e2b17d66
chinggg <24590067+chinggg@users.noreply.github.com>
2021-05-21 20:31:10 +0800

    feat(error): add TooManyReqError

--- error.py
+++ error.py
@@ -2,3 +2,10 @@ class LoginError(Exception):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    def __str__(self):
+        return f'{self.username} {self.password} is wrong'
+
+class TooManyReqError(Exception):
+    def __str__(self):
+        return 'You have sent too many requests'
